dfc491fc4ebbf16a73a98bb4b4d38da1d8584cce
Giulianalescano10 <lescanogiuliana65@gmail.com>
2020-09-21 18:23:35 -0300

    pull

--- backend/build/controllers/index.controller.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function mensaje_bienvenida(req, res) {
-    res.json("esta es la ruta principal de la pagina");
-}
-exports.mensaje_bienvenida = mensaje_bienvenida;
--- backend/src/controllers/index.controller.ts
+++
@@ -1,6 +0,0 @@
-import{ Response, Request} from 'express';
-
-export function mensaje_bienvenida(req:Request, res:Response)
-{
-    res.json("esta es la ruta principal de la pagina");
-}
