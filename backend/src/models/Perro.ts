f053a42180550bbb4606d8a6ee4759901a114ab3
Giulianalescano10 <lescanogiuliana65@gmail.com>
2020-10-13 19:01:45 -0300

    actualizacion

--- backend/src/models/Perro.ts
+++ backend/src/models/Perro.ts
@@ -1,8 +1,15 @@
 export interface ICanino
 {
     id?:number;
+    nombre:string;
+    fecha_nacimiento:Date;
     edad:number;
-    raza:string;
-    tamaño:string;
-    enfermedad:string;
+    sexo:string;
+    tamaño:boolean;
+    castrado:boolean;
+    desparasitado:boolean;
+	vacunado:boolean;
+	descripcion:Text;
+	estado_adopcion:string;
+	fecha_adopcion:Date
 }
