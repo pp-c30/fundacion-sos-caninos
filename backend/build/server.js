659d8213624d5a7967566935a8fecbf1350fc8ee
MelinaDiaz22 <diazm@cet30.edu.ar>
2020-10-22 01:58:05 -0300

    -

--- backend/build/server.js
+++ backend/build/server.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.server = void 0;
 const express_1 = __importDefault(require("express"));
 const index_routes_1 = __importDefault(require("./routes/index.routes"));
 const localidades_routes_1 = __importDefault(require("./routes/localidades.routes"));
