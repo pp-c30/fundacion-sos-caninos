0ec5a455c31d49095ed935a23b033d666d47f5ad
merinobruno <brunoezequiel.merino@gmail.com>
2020-11-10 02:03:54 -0300

    Tabla canino

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,17 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CaninoComponent } from './components/canino/canino.component';
-import { CategoriaDonacionComponent } from './components/categoria-donacion/categoria-donacion.component';
-import { DonacionesComponent } from './components/donaciones/donaciones.component';
-import { EventoComponent } from './components/evento/evento.component';
-import { FormularioAdopcionComponent } from './components/formulario-adopcion/formulario-adopcion.component';
-import { NavegacionComponent } from './components/navegacion/navegacion.component';
 
-import { HttpClientModule } from "@angular/common/http";
+import { CategoriaDonacionComponent } from './components/categoria-donacion/categoria-donacion.component';
+import { EventoComponent } from './components/evento/evento.component';
+import { NavegacionComponent } from './components/navegacion/navegacion.component';
+import { FormularioAdopcionComponent } from './components/formulario-adopcion/formulario-adopcion.component';
+import { DonacionesComponent } from './components/donaciones/donaciones.component';
+import { CaninoComponent } from './components/canino/canino.component';
 import { LocalidadesComponent } from './components/localidades/localidades.component';
 import { ProvinciaComponent } from './components/provincia/provincia.component';
 import { RequisitosComponent } from './components/requisitos/requisitos.component';
+
+import { HttpClientModule, HttpClient } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxPaginationModule } from "ngx-pagination";
+import { Ng2SearchPipeModule } from "ng2-search-filter";
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,7 +35,11 @@ import { RequisitosComponent } from './components/requisitos/requisitos.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    Ng2SearchPipeModule,
+    NgxPaginationModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/components/canino/canino.component.ts
+++ frontend/src/app/components/canino/canino.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CaninoService } from "../../service/canino.service";
+
+import { FormBuilder, FormGroup, Form, Validators } from "@angular/forms";
+import { ICanino } from 'src/app/modules/Canino';
+
 @Component({
   selector: 'app-canino',
   templateUrl: './canino.component.html',
@@ -7,9 +12,93 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CaninoComponent implements OnInit {
 
-  constructor() { }
+  listCanino = [];
+
+  formCanino: FormGroup;
+
+  buscarCanino:any;
+
+  p:number = 1;
+
+  constructor(private caninoServ:CaninoService, private fb: FormBuilder) {
+
+    this.formCanino = this.fb.group({
+
+      id_canino:[null],
+      nombre:["",[Validators.required,Validators.minLength(2)]],
+      fecha_nacimiento:["",[Validators.required,Validators.minLength(10)]],
+      edad:["",[Validators.required]],
+      sexo:["",[Validators.required]],
+      tamanio:["",[Validators.required]],
+      castrado:["",[Validators.required]],
+      desparasitado:["",[Validators.required]],
+      vacunado:["",[Validators.required]],
+      descripcion:["",[Validators.required]],
+      estado_adopcion:["",[Validators.required]],
+      fecha_adopcion:["",[Validators.required,Validators.minLength(10),Validators.maxLength(10)]]
+    });
+
+   }
 
   ngOnInit(): void {
+    this.obtenerCanino();
   }
+
+  obtenerCanino()
+  {
+    this.caninoServ.getCanino().subscribe(
+      resultado => this.listCanino = resultado,
+      error => console.log(error)
+    )
+  }
+
+  guardarCanino()
+  {
+
+    if(this.formCanino.value.id_canino)
+    {
+      this.caninoServ.updateCanino(this.formCanino.value).subscribe(
+        respuesta=> {
+          console.log(respuesta);
+          this.obtenerCanino();
+          this.formCanino.reset();
+        },
+        error => console.log(error)
+      )
+    }else{
+    //console.log(this.formCanino.value);
+    this.caninoServ.saveCanino(this.formCanino.value).subscribe(
+      resultado => {
+        console.log(resultado);
+        this.obtenerCanino();
+        this.formCanino.reset();
+      },
+      error => console.log(error)
+    );
+    }
+
+
+  }
+
+  editarCanino(canino:ICanino)
+  {
+    this.formCanino.setValue(canino);
+  }
+
+  eliminarCanino(id_canino:number)
+  {
+
+    if(confirm("¿Está seguro que desea ejecutar esta acción?"))
+    {
+      this.caninoServ.deleteCanino(id_canino).subscribe(
+        respuesta => {
+          console.log(respuesta);
+          this.obtenerCanino();
+        }, 
+        error => console.log(error)
+      )
+    }
+
+}
 
 }
