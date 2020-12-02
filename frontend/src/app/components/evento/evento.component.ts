c969cb7a6ca86f39354a03ffbfd640dbcd5583de
merinobruno <brunoezequiel.merino@gmail.com>
2020-12-02 19:20:30 -0300

    crud evento

--- frontend/src/app/components/evento/evento.component.ts
+++ frontend/src/app/components/evento/evento.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { EventoService } from "../../service/evento.service";
+
+import { FormBuilder, FormGroup, Form, Validators } from "@angular/forms";
+import { IEvento } from 'src/app/models/evento';
+
 @Component({
   selector: 'app-evento',
   templateUrl: './evento.component.html',
@@ -7,9 +12,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventoComponent implements OnInit {
 
-  constructor() { }
+  listEvento = [];
+
+  formEvento: FormGroup;
+
+  buscarEvento:any;
+
+  p:number = 1;
+
+  constructor(private eventoServ:EventoService, private fb: FormBuilder) {
+
+    this.formEvento = this.fb.group({
+
+      id_evento:[null],
+      titulo:["",[Validators.required,Validators.minLength(2)]],
+      descripcion:["",[Validators.required]],
+      contacto:["",[Validators.required]],
+      ubicacion:["",[Validators.required]],
+      fecha_hora:["",[Validators.required]]
+
+    });
+
+   }
 
   ngOnInit(): void {
+    this.obtenerEvento();
   }
+
+  obtenerEvento()
+  {
+    this.eventoServ.getEvento().subscribe(
+      resultado => this.listEvento = resultado,
+      error => console.log(error)
+    )
+  }
+
+  guardarEvento()
+  {
+
+    if(this.formEvento.value.id_evento)
+    {
+      this.eventoServ.updateEvento(this.formEvento.value).subscribe(
+        respuesta=> {
+          console.log(respuesta);
+          this.obtenerEvento();
+          this.formEvento.reset();
+        },
+        error => console.log(error)
+      )
+    }else{
+    //console.log(this.formEvento.value);
+    this.eventoServ.saveEvento(this.formEvento.value).subscribe(
+      resultado => {
+        console.log(resultado);
+        this.formEvento.reset();
+        this.obtenerEvento();
+      },
+      error => console.log(error)
+    );
+    }
+
+
+  }
+
+  editarEvento(evento:IEvento)
+  {
+    this.formEvento.setValue(evento);
+  }
+
+  eliminarEvento(id_evento:number)
+  {
+
+    if(confirm("¿Está seguro que desea ejecutar esta acción?"))
+    {
+      this.eventoServ.deleteEvento(id_evento).subscribe(
+        respuesta => {
+          console.log(respuesta);
+          this.obtenerEvento();
+        }, 
+        error => console.log(error)
+      )
+    }
+
+}
 
 }
