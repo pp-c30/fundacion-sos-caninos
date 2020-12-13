a665f0dcacea079b484de8252bd75bda7f1e2ea4
Giulianalescano10 <lescanogiuliana56@gmail.com>
2020-12-13 20:32:39 -0300

    actualizacion detalle canino public

--- frontend/src/app/components/detalle-canino-public/detalle-canino-public.component.ts
+++ frontend/src/app/components/detalle-canino-public/detalle-canino-public.component.ts
@@ -18,9 +18,25 @@ import { Router } from "@angular/router";
 export class DetalleCaninoPublicComponent implements OnInit {
 
 
+  selected:any;
+  filtered:any;
+
  id_canino:number;
 
  lista_caninos:ICanino[]=[];
+
+ caninos = [
+  { value: "All", id: "123" },
+  { value: "Unpaid and sent", id:"12" },
+
+  { value: "Unpaid and sent",id:"23" },
+  { value: "Unpaid and not sent" ,id:"45"},
+  { value: "Unpaid with due date",id:"56" },
+  { value: "Paid",id:"57" },
+  { value: "Open",id:"78" },
+  { value: "Overdue" ,id:"45"}];
+
+status = ['Select Status', 'All', 'Unpaid and sent', 'Unpaid with due date', 'Paid', 'Open', 'Overdue'];
 
 
 
@@ -44,5 +60,11 @@ export class DetalleCaninoPublicComponent implements OnInit {
         this.lista_caninos = resultado;
       }
     )
+  }
+
+  caninoSeleccionado ()
+  {
+    console.log(this.selected)
+    this.filtered = this.caninos.filter(t=>t.value ==this.selected)
   }
 }
