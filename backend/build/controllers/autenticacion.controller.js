f926f955647f358e846c38fd5bd90e2497948a4f
Giulianalescano10 <lescanogiuliana56@gmail.com>
2020-12-16 19:55:57 -0300

    correcciones

--- backend/build/controllers/autenticacion.controller.js
+++ backend/build/controllers/autenticacion.controller.js
@@ -12,7 +12,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.AutenticacionController = void 0;
 const database_1 = require("../database");
 const bcryptjs_1 = __importDefault(require("bcryptjs"));
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
